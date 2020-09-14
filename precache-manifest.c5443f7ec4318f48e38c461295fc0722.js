1cf54caf16c8a979c16bbb665770181a96bdaae1
Arzakan <arzakanporchkhidze17@mgail.com>
2020-09-14 17:50:23 +0400

    Updates

---
+++ precache-manifest.c5443f7ec4318f48e38c461295fc0722.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b991b35c8bba412c58191f7e9b5175ae",
+    "url": "/MyCalculator/index.html"
+  },
+  {
+    "revision": "9bb78c7191b143a5370a",
+    "url": "/MyCalculator/static/css/main.91c5a80b.chunk.css"
+  },
+  {
+    "revision": "ec7741ac53eb67f0941e",
+    "url": "/MyCalculator/static/js/2.7c96ed72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyCalculator/static/js/2.7c96ed72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bb78c7191b143a5370a",
+    "url": "/MyCalculator/static/js/main.41a201ec.chunk.js"
+  },
+  {
+    "revision": "85c8c9a9f9e001ce6d7f",
+    "url": "/MyCalculator/static/js/runtime-main.c6a2c967.js"
+  }
+]);
